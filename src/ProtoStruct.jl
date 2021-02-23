fadfde34735d2ee8a851ffca18fd28cf0e68ec18
Simon Christ <simonchrist@gmx.de>
2021-02-23 23:53:12 +0100

    extract field names and types, add postional ctor

--- src/ProtoStruct.jl
+++ src/ProtoStruct.jl
@@ -1,9 +1,24 @@
-macro proto( name )
-    ex = quote
-        struct $name{NT<:NamedTuple}
-            properties::NT
-        end # struct
+macro proto( expr )
+    if expr.head != Symbol("struct")
+        throw(ArgumentError("Expected expression to be a type definition."))
+    end
 
+    name = expr.args[2]
+    if !(name isa Symbol)
+        name = name.args[1]
+    end
+
+    field_names = Tuple(expr.args[3].args[i].args[1] for i in 2:2:length(expr.args[3].args))
+    field_types = quote Tuple{$((expr.args[3].args[i].args[2] for i in 2:2:length(expr.args[3].args))...)} end
+    
+    ex = quote
+            if !@isdefined $name
+                struct $name{NT<:NamedTuple}
+                    properties::NT
+                end # struct
+            end # if
+        
+        $name(args...) = $name(NamedTuple{$field_names, $field_types}(args))
         $name(;kwargs...) = $name(kwargs.data)
 
         function Base.getproperty( o::$name, s::Symbol )
@@ -16,3 +31,5 @@ macro proto( name )
     end # quote
     esc(ex)
 end # macro
+
+
--- test/test_ProtoStruct.jl
+++ test/test_ProtoStruct.jl
@@ -1,7 +1,12 @@
 using ProtoStructs, Test
 
-@proto TestMe
-test_me = @test_nowarn TestMe(A=1, B="2", C=complex(1))
+@proto struct TestMe{T}
+    A::Int
+    B
+    C::T
+end
+test_me = @test_nowarn TestMe(1, "2", complex(1))
+test_me_kw = @test_nowarn TestMe(A=1, B="2", C=complex(1))
 
 @testset "Construction" begin
     @test TestMe((A=1,)) isa TestMe
@@ -16,4 +21,19 @@ end # testset
 
 @testset "Properties" begin
     @test propertynames( test_me ) == (:A, :B, :C)
+end # testset
+
+@proto struct TestMe{T}
+    A::Int
+    B
+    C::T
+    D::T
+end
+
+test_me = @test_nowarn TestMe(1, "2", complex(1))
+test_me_kw = @test_nowarn TestMe(A=1, B="2", C=complex(1))
+
+@testset "Redefinition" begin
+    @test TestMe((A=1,)) isa TestMe
+    @test TestMe(A=1) == TestMe((A=1,))
 end # testset
