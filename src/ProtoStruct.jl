60e1dbaa3e6f4e9a05b2d9dc99232f8d576ff5e3
Simon Christ <simonchrist@gmx.de>
2021-02-24 21:10:41 +0100

    Fix Int and Any handling

--- src/ProtoStruct.jl
+++ src/ProtoStruct.jl
@@ -8,8 +8,16 @@ macro proto( expr )
         name = name.args[1]
     end
 
-    field_names = Tuple(expr.args[3].args[i].args[1] for i in 2:2:length(expr.args[3].args))
-    field_types = quote Tuple{$((expr.args[3].args[i].args[2] for i in 2:2:length(expr.args[3].args))...)} end
+    field_info = map(expr.args[3].args[2:2:length(expr.args[3].args)]) do field
+                        return if field isa Symbol
+                            (field, Any)
+                        else
+                            (field.args[1], field.args[2])
+                        end
+                    end
+    field_names = Tuple(getindex.(field_info, 1))
+    # Symbol for Int -> Int64/Int32
+    field_types = quote Tuple{$(Symbol.(getindex.(field_info, 2))...)} end
     
     ex = quote
             if !@isdefined $name
@@ -18,17 +26,17 @@ macro proto( expr )
                 end # struct
             end # if
         
-        $name(args...) = $name(NamedTuple{$field_names, $field_types}(args))
-        $name(;kwargs...) = $name(kwargs.data)
+            $name(args...) = $name(NamedTuple{$field_names, $field_types}(args))
+            $name(;kwargs...) = $name(kwargs.data)
 
-        function Base.getproperty( o::$name, s::Symbol )
-            return getproperty( getfield(o, :properties), s )
-        end # function
+            function Base.getproperty( o::$name, s::Symbol )
+                return getproperty( getfield(o, :properties), s )
+            end # function
 
-        function Base.propertynames( o::$name )
-            return propertynames( getfield(o, :properties) )
-        end # function
-    end # quote
+            function Base.propertynames( o::$name )
+                return propertynames( getfield(o, :properties) )
+            end # function
+        end # quote
     esc(ex)
 end # macro
 
