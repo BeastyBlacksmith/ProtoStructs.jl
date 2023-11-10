a4fe81d7ecb99d4714073751cd1d823271a1e192
Steffen Plunder <steffen.plunder@web.de>
2023-11-10 15:39:43 +0900

    fix syntax error and printing of symbols

--- test/test_ProtoStruct.jl
+++ test/test_ProtoStruct.jl
@@ -161,7 +161,7 @@ end
         @test cf.A == 1
         @test cf.B == :no
         @test cf.C == 3
-        @test (show(c); true)  # test if show method works without errors
+        @test_nowarn show(cf)
         cf.A = 5
         @test_throws ErrorException cf.B = :yes
         @test_throws ErrorException cf.C = 5
